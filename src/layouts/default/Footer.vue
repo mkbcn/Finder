cc8e1c96840b515fa05fd5673003299032596fd2
Kaveesha <kalshandhanasekara@gmail.com>
2023-05-29 14:33:27 +0530

    add Footer

---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,37 @@
+<script>
+  export default {
+    name:"Footer",
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
+</script>
+
+<template>
+  <v-footer class="bg-teal d-flex flex-column justify-end text-center">
+    <div >
+
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="plain"
+        size="small"
+      ></v-btn>
+    </div>
+
+    <div class="px-4 py-2 bg-black text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </div>
+  </v-footer>
+</template>
+
+<style scoped>
+
+</style>
