7a88b38f382d35323fb40f2d5402a79d9dd5b96d
Kaveesha <kalshandhanasekara@gmail.com>
2023-05-29 14:32:41 +0530

    add Titles component

---
+++ src/components/Titles.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "Titles",
+  props:{
+    title:null,
+    subtitle:null
+  }
+}
+</script>
+
+<template>
+  <h1 class="ml-5">{{title}}</h1>
+  <h2>{{subtitle}}</h2>
+</template>
+
+<style scoped>
+
+</style>
