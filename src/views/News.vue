ee9c45512decf7a64be20bbf9e92123c97d84761
Kaveesha <kalshandhanasekara@gmail.com>
2023-05-29 14:36:08 +0530

    add news page

---
+++ src/views/News.vue
@@ -0,0 +1,16 @@
+<script >
+import Titles from "@/components/Titles.vue";
+
+export default {
+  name:"News",
+  components: {Titles}
+}
+</script>
+
+<template >
+  <Titles title="News"/>
+</template>
+
+<style scoped>
+
+</style>
